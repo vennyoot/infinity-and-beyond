b7000ff215b8f42f070c3be8aea91a0e8b493560
Zoe C. | Venn <khoyox@gmail.com>
2021-03-25 21:21:58 -0400

    LightOverseer.cs & Mirror.cs scripts initialized

---
+++ Constellation/Assets/Scripts/LightOverseer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOverseer : MonoBehaviour
+{
+    private LineRenderer lineRender;
+    private List<Vector2> lineLink;
+}
